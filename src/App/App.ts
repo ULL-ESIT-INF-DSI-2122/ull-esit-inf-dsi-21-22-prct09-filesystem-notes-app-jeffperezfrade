1e7fddb7c004c1c0f5d3ddc4f9f85fdd09d6126d
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-22 10:16:16 +0100

    yargs modify note

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,7 +1,12 @@
 import * as yargs from 'yargs';
 import {TextNotes, colors} from '../TextNotes/TextNotes.class';
+/**
+ * TextNotes object.
+ */
 const textNotes: TextNotes = TextNotes.getNotes();
-
+/**
+ * Adding new note command line.
+ */
 yargs.command({
   command: 'add',
   describe: 'Add new note',
@@ -42,6 +47,52 @@ yargs.command({
         }
       });
       textNotes.addNote(argv.user, argv.title, argv.body, noteColor);
+    }
+  },
+});
+/**
+ * Modify notes command line.
+ */
+yargs.command({
+  command: 'modify',
+  describe: 'Modify note',
+  builder: {
+    user: {
+      describe: 'User name',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+    color: {
+      describe: 'Note color',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    // Default color: blue.
+    let noteColor: colors = colors.blue;
+    if (
+      typeof argv.color == 'string' &&
+      typeof argv.body == 'string' &&
+      typeof argv.title == 'string' &&
+      typeof argv.user == 'string'
+    ) {
+      Object.values(colors).forEach((color) => {
+        if (argv.color == color) {
+          noteColor = color;
+        }
+      });
+      textNotes.modifyNote(argv.user, argv.title, argv.body, noteColor);
     }
   },
 });
